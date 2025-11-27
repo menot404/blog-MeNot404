3c767e856fe71592ec22f481d663e461f22245f0
Me_Not404 <m3not404@gmail.com>
2025-11-27 00:46:53 +0000

    Feat/delete update (#8)
    * feat(delete): implement feat delete a blog
    
    * feat(update): implem faet update

--- server/routes/blogRout.js
+++ server/routes/blogRout.js
@@ -12,8 +12,15 @@ router.get("/", blogController.getAllBlogs);
 // Route to get a single blog by ID
 router.get("/:id", blogController.getBlogById);
 
+// Route to update a single blog by ID
+// accept multipart form with optional 'media' file for updates
+router.post("/:id", upload.single("media"), blogController.updateOnBlog);
+
 // Route to create a new blog
 // apply multer to the POST handler so form multipart data (including file) is parsed
 router.post("/", upload.single("media"), blogController.createBlog);
+
+// Route to delete a blog by ID
+router.post("/:id/delete", blogController.deleteBlogById);
 
 module.exports = router;
