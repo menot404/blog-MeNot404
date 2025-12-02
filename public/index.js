965016c288da34a0215181492513abb526bc2400
Me_Not404 <m3not404@gmail.com>
2025-12-02 00:10:27 +0000

    Responsive/header -  implement menu for mobiles devices
    * responsive design is being implemented
    
    * add menu burger - mobile-nav-toggle
    
    * implement button close
    
    * implement menu for mobiles devices

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     const mobilNavToggle = document.querySelector('body > header > div.mobil > button');
+    const mainNav = document.querySelector("body > header > nav")
     const functionNavToggle = ()=>{
-            //mainNav.classList.toggle('nav-open')
+            mainNav.classList.toggle('nav-open')
             mobilNavToggle.classList.toggle('is-active')
             //mainNav.appendChild(headerAction);
             console.log("click test")
